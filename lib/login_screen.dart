07f4ff6f0c4607bd647d1216a2d925e3d9766b2b
Marwanhoo <muh.marwanhoo@gmail.com>
2024-06-04 04:51:01 +0300

    Design Login Screen

---
+++ lib/login_screen.dart
@@ -0,0 +1,113 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+      ),
+      extendBodyBehindAppBar: true,
+      body: Stack(
+        children: [
+          Image.network(
+            'https://images.unsplash.com/photo-1564951434112-64d74cc2a2d7?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            fit: BoxFit.cover,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                textAlign: TextAlign.center,
+                "My \n Gellary",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 50,
+                  fontWeight: FontWeight.bold,
+                  height: 0.0,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(32),
+                child: ClipRect(
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 35,vertical: 35),
+                      decoration: BoxDecoration(
+                          color: Colors.white.withOpacity(0.3),
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(30))),
+                      child: Column(
+                        children: [
+                          const Text(
+                            "LOG IN",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30,
+                            ),
+                          ),
+                          const SizedBox(height: 30),
+                          TextFormField(
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(35),
+                                borderSide: BorderSide.none,
+                              ),
+                              fillColor: Colors.grey.shade200,
+                              filled: true,
+                              hintText: "User Name",
+                              contentPadding: const EdgeInsets.symmetric(horizontal: 20),
+                            ),
+                          ),
+                          const SizedBox(height: 30),
+                          TextFormField(
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(35),
+                                borderSide: BorderSide.none,
+                              ),
+                              fillColor: Colors.grey.shade200,
+                              filled: true,
+                              hintText: "Password",
+                              contentPadding: const EdgeInsets.symmetric(horizontal: 20),
+                            ),
+                          ),
+                          const SizedBox(height: 30),
+                          ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Color(0xff7bb3ff),
+                              foregroundColor: Colors.white,
+                              minimumSize: Size(double.infinity, 50),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(14),
+                              ),
+                            ),
+
+                            onPressed: () {},
+                            child: Text(
+                              "SUBMIT",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
